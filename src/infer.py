ec91fbe325b8aefdde81e4d3d5f2bfc45208a1be
Ogrnz <1781589+ogrnz@users.noreply.github.com>
2024-05-01 12:24:44 +0200

    refactor: CLIze infer.py

--- src/infer.py
+++ src/infer.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 from transformers import AutoTokenizer, pipeline
 
@@ -15,15 +16,48 @@ def load_messages(file_path):
     return messages
 
 
-if __name__ == "__main__":
-    messages = load_messages("./data/test.jsonl")
+def main(args):
+    tokenizer = AutoTokenizer.from_pretrained(args.checkpoint)
+    summarizer = pipeline("summarization", model=args.checkpoint, tokenizer=tokenizer)
 
-    checkpoint = "./results/checkpoint-20000"
-    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-    summarizer = pipeline("summarization", model=checkpoint, tokenizer=tokenizer)
+    if args.message:  # Directly use the message string provided
+        messages = [args.message]
+    else:  # Load messages from file if path is provided
+        messages = load_messages(args.file_path)
 
     for message in messages:
         print("=" * 10)
         title = generate_title(message, summarizer)[0]["summary_text"]
-        print(f"{title}:\n {message}\n")
-        print("=" * 10)
+        print(f"{title}:\n{message}\n")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Generate summaries from texts using a transformer model."
+    )
+    parser.add_argument(
+        "-c",
+        "--checkpoint",
+        type=str,
+        default="./results/checkpoint-20000",
+        help="Model checkpoint path or compatible huggingface model url",
+    )
+    parser.add_argument(
+        "-f",
+        "--file_path",
+        type=str,
+        default="./data/test.jsonl",
+        help="(optional) Path to a file containing JSONL messages",
+    )
+    parser.add_argument(
+        "-m",
+        "--message",
+        type=str,
+        help="(optional) Direct message string to summarize",
+    )
+    args = parser.parse_args()
+
+    if not args.file_path and not args.message:
+        parser.error("Either --file_path or --message must be provided")
+
+    main(args)
